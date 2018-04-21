9db4de1a17d237cd7d5b957a6f3ba3b0b0cb92ea
Dj Art <art.letter.mail@gmail.com>
2018-04-22 00:10:11 +0800

    SFix

--- EmptyBox.IO/Network/IP/IPPort.cs
+++ EmptyBox.IO/Network/IP/IPPort.cs
@@ -2,6 +2,16 @@
 {
     public struct IPPort : IPort
     {
+        public static implicit operator IPPort(ushort value)
+        {
+            return new IPPort(value);
+        }
+
+        public static implicit operator ushort(IPPort value)
+        {
+            return value.Value;
+        }
+
         public ushort Value { get; set; }
 
         public IPPort(ushort value)
