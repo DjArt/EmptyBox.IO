addf60af379f2a48c83cb0c1c9fa4155c381dc39
Dj Art <art.letter.mail@gmail.com>
2018-05-05 20:39:52 +0800

    Project sheme added

--- EmptyBox.IO/Devices/Audio/IAudioDevice.cs
+++ EmptyBox.IO/Devices/Audio/IAudioDevice.cs
@@ -1,11 +1,20 @@
-﻿using System;
+﻿using EmptyBox.IO.Access;
+using EmptyBox.ScriptRuntime;
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace EmptyBox.IO.Devices.Audio
 {
     public interface IAudioDevice : IDevice
     {
+        byte BitDepth { get; }
+        uint SampleRate { get; }
+        float Volume { get; }
+        IEnumerable<byte> SupportedBitDepths { get; }
+        IEnumerable<uint> SupportedSampleRates { get; }
 
+        Task<VoidResult<AccessStatus>> TrySetSampleRate(uint rate);
+        Task<VoidResult<AccessStatus>> TrySetBitDepth(byte depth);
+        Task<VoidResult<AccessStatus>> TrySetVolume(float volume);
     }
 }
