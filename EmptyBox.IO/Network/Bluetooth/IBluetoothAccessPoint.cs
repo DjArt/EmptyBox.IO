993746c054b80d65d9134d1a8bc689febfd330ce
DjArt <art.letter.mail@gmail.com>
2019-11-13 21:30:29 +0700

    IBluetoothAccessPoint added

---
+++ EmptyBox.IO/Network/Bluetooth/IBluetoothAccessPoint.cs
@@ -0,0 +1,14 @@
+﻿using EmptyBox.IO.Devices.Bluetooth;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmptyBox.IO.Network.Bluetooth
+{
+    public interface IBluetoothAccessPoint<out TPort> : IAccessPoint<IBluetoothDevice, TPort>
+        where TPort : IPort
+    {
+        BluetoothMode Mode { get; }
+        BluetoothProtocol Protocol { get; }
+    }
+}
