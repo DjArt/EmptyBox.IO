4bb06dfe21fe7d719355f33ae5c89eb045f21a4d
DjArt <art.letter.mail@gmail.com>
2020-01-14 23:41:24 +0700

    IPEndPoint to IPAccessPoint conversion fix

--- EmptyBox.IO/Interoperability/Extensions.cs
+++ EmptyBox.IO/Interoperability/Extensions.cs
@@ -1,6 +1,8 @@
 ﻿using EmptyBox.IO.Network;
 using EmptyBox.IO.Network.IP;
 using System.Net;
+using System;
+using System.Net.Sockets;
 
 namespace EmptyBox.IO.Interoperability
 {
@@ -13,7 +15,21 @@ namespace EmptyBox.IO.Interoperability
 
         public static IPAccessPoint ToIPAccessPoint(this IPEndPoint endPoint)
         {
-            return new IPAccessPoint(new Network.IP.IPAddress(endPoint.Address.GetAddressBytes()), new IPPort((ushort)endPoint.Port));
+            int size = 0;
+            switch (endPoint.AddressFamily)
+            {
+                case AddressFamily.InterNetwork:
+                    size = Network.IP.IPAddress.Length4;
+                    break;
+                case AddressFamily.InterNetworkV6:
+                    size = Network.IP.IPAddress.Length6;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            byte[] address = new byte[size];
+            Array.Copy(endPoint.Address.GetAddressBytes(), 0, address, 0, size);
+            return new IPAccessPoint(new Network.IP.IPAddress(address), new IPPort((ushort)endPoint.Port));
         }
 
         public static uint? ToUInt(this int? value)
