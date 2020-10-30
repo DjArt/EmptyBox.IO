85974ce2ff32c52b6104800395eee8f9eb67908a
djart <luke_wide@outlook.com>
2020-10-30 08:58:21 +0700

    Some interop things

---
+++ EmptyBox.IO.Media.OpenCV/Interoperability/FrameExtensions.cs
@@ -0,0 +1,29 @@
+﻿using EmptyBox.IO.Media.Frames;
+using EmptyBox.IO.Media.OpenCV.Frames;
+using OpenCvSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmptyBox.IO.Media.OpenCV.Interoperability
+{
+    public static class FrameExtensions
+    {
+        public static Mat ToMat(this IVideoFrame frame)
+        {
+            if (frame is VideoFrame ocvFrame)
+            {
+                return ocvFrame.NativeFrame;
+            }
+            else
+            {
+                Mat result;
+                unsafe
+                {
+                    result = new Mat((int)frame.EncodingProperties.Resolution.Height, (int)frame.EncodingProperties.Resolution.Width, default, new IntPtr(frame.GetBuffer()));
+                }
+                return result;
+            }
+        }
+    }
+}
---
+++ EmptyBox.IO.Media.OpenCV/Interoperability/SizeExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmptyBox.IO.Media.OpenCV.Interoperability
+{
+    public static class SizeExtensions
+    {
+        public static OpenCvSharp.Size ToOcvSize(this Foundation.Size size)
+        {
+            return new OpenCvSharp.Size(size.Width, size.Height);
+        }
+    }
+}
